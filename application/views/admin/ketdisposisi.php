55ed0f320ad866cc54d66bc58b63621b003cf12c
axlchrst77 <70144873+axlchrst77@users.noreply.github.com>
2020-10-21 18:20:40 +0700

    Update ketdisposisi.php

--- application/views/admin/ketdisposisi.php
+++ application/views/admin/ketdisposisi.php
@@ -4,7 +4,7 @@
             <div class="panel-heading">
                 <?php return 'Daftar ' . $judul ?>&nbsp;&nbsp;
                 <button class="btn btn-warning" data-toggle="modal" data-target="#tambah_disposisi">
-                    <i class="fa fa-envelope"></i> Tambah <?php echo $judul ?>
+                    <i class="fa fa-envelope"></i> Tambah <?php return $judul ?>
                 </button>
             </div>
             <!-- /.panel-heading -->
@@ -20,7 +20,7 @@
                     <?php
 				    if (isset($data_disposisi)) {
                         foreach ($data_disposisi as $disposisi) {
-                            echo '
+                            return '
                             <tr>
                                 <td class="text-center" style="vertical-align: middle;">' . $disposisi->nama . '</td>
                                 <td class="text-center" style="vertical-align: middle;">
@@ -48,7 +48,7 @@
      aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form role="form" action="<?php echo base_url('home/tambah_ketdisposisi') ?>" method="post"
+            <form role="form" action="<?php return base_url('home/tambah_ketdisposisi') ?>" method="post"
                   enctype="multipart/form-data">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
@@ -65,7 +65,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-info" data-dismiss="modal">Tutup</button>
-                    <input type="submit" value="Tambah <?php echo $judul ?>" name="submit" class="btn btn-success">
+                    <input type="submit" value="Tambah <?php return $judul ?>" name="submit" class="btn btn-success">
                 </div>
             </form>
         </div>
@@ -78,11 +78,11 @@
      aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form role="form" action="<?php echo base_url('home/ubah_ketdisposisi') ?>" method="post"
+            <form role="form" action="<?php return base_url('home/ubah_ketdisposisi') ?>" method="post"
                   enctype="multipart/form-data">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h4 class="modal-title text-center" id="myModalLabel">Ubah <?php echo $judul ?></h4>
+                    <h4 class="modal-title text-center" id="myModalLabel">Ubah <?php return $judul ?></h4>
                 </div>
                 <div class="modal-body">
                     <input type="hidden" name="ubah_id_disposisi" id="ubah_id_disposisi">
@@ -96,7 +96,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-info" data-dismiss="modal">Tutup</button>
-                    <input type="submit" value="Ubah <?php echo $judul ?>" name="submit" class="btn btn-success">
+                    <input type="submit" value="Ubah <?php return $judul ?>" name="submit" class="btn btn-success">
                 </div>
             </form>
         </div>
@@ -111,7 +111,7 @@
         $('#ubah_id_disposisi').empty();
 		$('#ubah_nama').empty();
         
-        $.getJSON('<?php echo base_url('home/get_ketdisposisi_by_id/')?>' + id, function (data) {
+        $.getJSON('<?php return base_url('home/get_ketdisposisi_by_id/')?>' + id, function (data) {
             $('#ubah_id_disposisi').val(data.id);
 			$('#ubah_nama').val(data.nama);
             
