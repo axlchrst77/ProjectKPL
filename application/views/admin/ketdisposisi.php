f505aa178c6064e12531b3c7ac7d130dd090c781
axlchrst77 <70144873+axlchrst77@users.noreply.github.com>
2020-10-21 18:25:52 +0700

    Update ketdisposisi.php

--- application/views/admin/ketdisposisi.php
+++ application/views/admin/ketdisposisi.php
@@ -52,7 +52,7 @@
                   enctype="multipart/form-data">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h4 class="modal-title text-center" id="myModalLabel">Tambah <?php echo $judul ?></h4>
+                    <h4 class="modal-title text-center" id="myModalLabel">Tambah <?php return $judul ?></h4>
                 </div>
                 <div class="modal-body">
                     
