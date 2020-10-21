7a0e002b1d4753258d73b626af5183d350843c3e
axlchrst77 <70144873+axlchrst77@users.noreply.github.com>
2020-10-21 21:47:12 +0700

    Update error_exception.php

--- application/views/errors/html/error_exception.php
+++ application/views/errors/html/error_exception.php
@@ -1,15 +1,15 @@
 <?php
-defined('BASEPATH') OR die('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
     <h4>An uncaught Exception was encountered</h4>
 
-    <p>Type: <?php echo get_class($exception); ?></p>
-    <p>Message: <?php echo $message; ?></p>
-    <p>Filename: <?php echo $exception->getFile(); ?></p>
-    <p>Line Number: <?php echo $exception->getLine(); ?></p>
+    <p>Type: <?php return get_class($exception); ?></p>
+    <p>Message: <?php return $message; ?></p>
+    <p>Filename: <?php return $exception->getFile(); ?></p>
+    <p>Line Number: <?php return $exception->getLine(); ?></p>
 
     <?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE): ?>
 
@@ -19,9 +19,9 @@ defined('BASEPATH') OR die('No direct script access allowed');
             <?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0): ?>
 
                 <p style="margin-left:10px">
-                    File: <?php echo $error['file']; ?><br/>
-                    Line: <?php echo $error['line']; ?><br/>
-                    Function: <?php echo $error['function']; ?>
+                    File: <?php return $error['file']; ?><br/>
+                    Line: <?php return $error['line']; ?><br/>
+                    Function: <?php return $error['function']; ?>
                 </p>
             <?php endif ?>
 
