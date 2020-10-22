1a2dc777bba01bc4c7fff7e6e307a8e6d30400fd
axlchrst77 <70144873+axlchrst77@users.noreply.github.com>
2020-10-22 09:57:02 +0700

    Update db_surat1.sql

--- db_surat1.sql
+++ db_surat1.sql
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE disposisi(
-  id_disposisi int(11) NOT NULL,
+  id_disposisi int NOT NULL,
   id_surat int(11) NOT NULL,
   id_pegawai_pengirim int(11) NOT NULL,
   id_pegawai_penerima int(11) NOT NULL,
