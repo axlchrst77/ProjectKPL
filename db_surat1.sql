c1a889bee1202903897af20f0688cb22fc9ada3b
axlchrst77 <70144873+axlchrst77@users.noreply.github.com>
2020-10-22 10:51:57 +0700

    Update db_surat1.sql

--- db_surat1.sql
+++ db_surat1.sql
@@ -277,8 +277,8 @@ INSERT INTO 'surat_masuk' ('id_surat', 'nomor_surat', 'tgl_kirim', 'tgl_terima',
 --
 ALTER TABLE disposisi
   ADD PRIMARY KEY (id_disposisi),
-  ADD KEY 'id_surat' (id_surat),
-  ADD KEY 'id_pegawai_penerima' (id_pegawai_penerima);
+  ADD KEY id_surat (id_surat),
+  ADD KEY id_pegawai_penerima (id_pegawai_penerima);
 
 --
 -- Indeks untuk tabel 'disposisi_memo'
